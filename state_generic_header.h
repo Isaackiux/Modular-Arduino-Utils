6729da138ab133fed57a37b11497529298de1fad
Isaackiux <116937683+Isaackiux@users.noreply.github.com>
2025-12-12 22:48:30 -0800

    Update state_generic_header.h

--- state_generic_header.h
+++ state_generic_header.h
@@ -1,1 +1,9 @@
+// state_generic_header.h
+#ifndef STATE_GENERIC_HEADER_H
+#define STATE_GENERIC_HEADER_H
 
+#include "utils_stateMachine_header.h"
+
+State createGenericState(); // Cambiar el nombre al de tu respectivo estado (el nombre de esta funcion, tiene que ser identico al de tu .cpp)
+
+#endif
